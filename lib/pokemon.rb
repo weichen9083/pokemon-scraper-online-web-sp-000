d79543696c0665677221a45e38eef7b2f7eabd2e
wei chen <weichen9083@gmail.com>
2021-03-19 03:34:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,26 +1,38 @@
 class Pokemon
-  attr_accessor :name, :type, :db, :id, :hp
-
-  def initialize(keywords)
-
-  end
-
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end 
+  
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
-  end
-
+    sql = <<-SQL 
+    INSERT INTO pokemon (name, type) VALUES (?,?)
+    SQL
+    
+    db.execute(sql,name, type)
+    
+  end 
+  
+  
   def self.find(num, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-    new_pokemon.hp = pokemon[0][3]
-    return new_pokemon
-  end
+    
+    sql = <<-SQL 
+    SELECT * FROM pokemon WHERE id = ? 
+    SQL
 
-  def alter_hp(num, db)
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
-    self.hp = num
-  end
-end
+  new_pokemon = db.execute(sql, num)  
+  
+  pokemonX = self.new(id:new_pokemon[0][0], name:new_pokemon[0][1], type:new_pokemon[0][2], db:new_pokemon)
+  
+  pokemonX
+  
+  
+
+end 
+
+end 
